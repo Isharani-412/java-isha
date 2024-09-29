b1c4893c8fc6f5de7f1bb0a7ddb34ada31f73f88
Isharani-412 <147904928+Isharani-412@users.noreply.github.com>
2024-09-29 09:18:25 +0530

    Initial commit

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+class Welcome
+{
+public static void main(String arg[])
+{
+	int a=10,b=20;
+	int sum =a+b;
+	System.out.println("sum="+sum);
+}
+}
---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+int a,b,sum;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter 2 no.s:");
+a=sc.nextInt();
+b=sc.nextInt();
+sum=a+b;
+System.out.println("sum="+sum);
+}
+}
